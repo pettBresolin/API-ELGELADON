62f453ff4466bde658877b7a69989248fb6e9397
Bresolin <pettbresolin@hotmail.com>
2022-12-21 20:23:26 -0300

    install database

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/routes/paleta.route');
+const routes = require('./src/routes/paleta.routes.js');
 
 const port = 3000;
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use('/paletas', routes);
 
 app.listen(port, () => {
