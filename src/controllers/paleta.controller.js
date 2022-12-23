35c3688440361f00713109bfa0969883b934e509
Bresolin <pettbresolin@hotmail.com>
2022-12-22 23:35:31 -0300

    add middleware

--- src/controllers/paleta.controller.js
+++ src/controllers/paleta.controller.js
@@ -1,25 +1,15 @@
 const paletasService = require('../services/paleta.service.js');
-const mongose = require('mongoose');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 
 const findAllPaletasController = async (req, res) => {
   const allPaletas = await paletasService.findAllPaletasService();
-
-  if (allPaletas.length == 0) {
-    return res.status(404).send({ message: 'Não existe nada cadastrado!' });
-  }
 
   res.send(allPaletas);
 };
 
 const findByIdPaletaController = async (req, res) => {
   const idParam = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    return res.status(404).send({ message: 'Id inválido!' });
-  }
   const chosenPaleta = await paletasService.findByIdPaletaService(idParam);
-
   if (!chosenPaleta) {
     return res.status(404).send({ message: 'Paleta não encontrada!' });
   }
@@ -28,13 +18,6 @@ const findByIdPaletaController = async (req, res) => {
 
 const createPaletaController = async (req, res) => {
   const paleta = req.body;
-
-  if (!paleta.sabor || !paleta.descricao || !paleta.foto || !paleta.preco) {
-    return res
-      .status(400)
-      .send({ message: 'Preencha todos os campos para enviar!' });
-  }
-
   const newPaleta = await paletasService.createPaletaService(paleta);
   res.send(newPaleta);
 };
@@ -42,33 +25,16 @@ const createPaletaController = async (req, res) => {
 const updatePaletaController = async (req, res) => {
   const idParam = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    return res.status(404).send({ message: 'Id inválido!' });
-  }
-
   const editPaleta = req.body;
-
-  if (
-    !editPaleta.sabor ||
-    !editPaleta.descricao ||
-    !editPaleta.foto ||
-    !editPaleta.preco
-  ) {
-    return res
-      .status(400)
-      .send({ message: 'Preencha todos os campos para enviar!' });
-  }
-
-  const updatePaleta = await paletasService.updatePaletaService(idParam, paletaEdit);
+  const updatePaleta = await paletasService.updatePaletaService(
+    idParam,
+    editPaleta,
+  );
   res.send(updatePaleta);
 };
 
 const deletePaletaController = async (req, res) => {
   const idParam = req.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(idParam)) {
-    return res.status(404).send({ message: 'Id inválido!' });
-  }
 
   await paletasService.deletePaletaService(idParam);
 
---
+++ src/middlewares/paleta.middleware.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const validId = (req, res, next) => {
+  const idParam = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    return res.status(400).send({ message: 'Id inválido!' });
+  }
+  next();
+};
+
+const validObjectBody = (req, res, next) => {
+  const editPaleta = req.body;
+  if (
+    !editPaleta.sabor ||
+    !editPaleta.descricao ||
+    !editPaleta.foto ||
+    !editPaleta.preco
+  ) {
+    return res
+      .status(400)
+      .send({ message: 'Preencha todos os campos para enviar!' });
+  }
+  next();
+};
+
+module.exports = {
+  validId,
+  validObjectBody,
+};
