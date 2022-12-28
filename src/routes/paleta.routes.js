03025900cc0e88e75c0bf3f7ab82a938f007ca8b
Bresolin <pettbresolin@hotmail.com>
2022-12-27 22:12:23 -0300

    config swagger

--- src/routes/paleta.routes.js
+++ src/routes/paleta.routes.js
@@ -1,9 +1,16 @@
 const route = require('express').Router();
 const controllerPaletas = require('../controllers/paleta.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
 const {
   validId,
   validObjectBody,
 } = require('../middlewares/paleta.middleware');
+
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get('/all-paletas', controllerPaletas.findAllPaletasController);
 route.get(
